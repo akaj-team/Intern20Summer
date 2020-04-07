c7b786569630d41636d3aa0c3753a12580b0ef1d
Quynh Ho T. T <59002138+at-QuynhHo@users.noreply.github.com>
2020-04-07 10:49:32 +0700

    Exercise Layout and View

---
+++ app/src/main/java/com/asiantech/summer/CheckEmailActivity.kt
@@ -0,0 +1,12 @@
+package com.asiantech.summer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckEmailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_email)
+    }
+}
