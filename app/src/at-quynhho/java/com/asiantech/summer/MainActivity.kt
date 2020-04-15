9a9e54dae8862e8e5d5456dd18c817632da631a4
Quynh Ho T. T <59002138+at-QuynhHo@users.noreply.github.com>
2020-04-16 00:17:45 +0700

    Exercise recyclerView

--- app/src/at-quynhho/java/com/asiantech/summer/MainActivity.kt
+++ app/src/at-quynhho/java/com/asiantech/summer/MainActivity.kt
@@ -2,11 +2,16 @@ package com.asiantech.summer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.asiantech.summer.fragmentLayout.MenuMessageFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val menuMessageFragment = MenuMessageFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.menu_view, menuMessageFragment, null)
+            .commit()
     }
 }
