642c918cf09fd2ac90dca29cc27cc89bdb16a594
Quynh Ho T. T <59002138+at-QuynhHo@users.noreply.github.com>
2020-04-23 14:31:08 +0700

    ex

--- app/src/at-quynhho/java/com/asiantech/summer/MainActivity.kt
+++ app/src/at-quynhho/java/com/asiantech/summer/MainActivity.kt
@@ -3,7 +3,7 @@ package com.asiantech.summer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ScreenColorActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
